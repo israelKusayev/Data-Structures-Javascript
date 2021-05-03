ee550c0ed02294561b6fa3be6042b844321d2f30
Israel kusayev <israel.kusayev@gmail.com>
2021-05-03 22:37:16 +0300

    add priority queue

--- queue.js
+++ queue.js
@@ -44,6 +44,54 @@ class ArrayQueue {
   }
 }
 
+class PriorityQueue {
+  #array;
+  #first = 0;
+  #rear = 0;
+  #count = 0;
+
+  constructor(count) {
+    this.#array = new Array(count);
+  }
+  enqueue(item) {
+    if (this.isFull()) throw new Error("queue is full");
+
+    for (let i = 0; i < this.#count; i++) {
+      const element = this.#array[i];
+      // if(item< )
+    }
+    this.#array[this.#rear] = item;
+
+    this.#rear = (this.#rear + 1) % this.#array.length;
+    this.#count++;
+  }
+  dequeue() {
+    if (this.isEmpty()) throw new Error("queue is empty");
+
+    const firstItem = this.#array[this.#first];
+    this.#array[this.#first] = undefined;
+    this.#first = (this.#first + 1) % this.#array.length;
+    this.#count--;
+
+    return firstItem;
+  }
+  peek() {
+    if (this.isEmpty()) throw new Error("queue is empty");
+
+    return this.#array[this.#first];
+  }
+  isEmpty() {
+    return this.#count === 0;
+  }
+  isFull() {
+    return this.#count === this.#array.length;
+  }
+
+  toString() {
+    return this.#array.slice(this.#first, this.#rear);
+  }
+}
+
 class QueueWithTwoStacks {
   #first;
   #second;
